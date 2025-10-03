594676f2cd6cfc653250e43c2f2d25df124532ad
wesley-cheung-ics4ui-01-25 <166552362+wesley-cheung-ics4ui-01-25@users.noreply.github.com>
2025-10-03 17:30:33 +0000

    update practiceproblems

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,43 @@
+import java.io.*;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
+	}
+	public static String readFile(String filename) {
+		FileReader inputStream = null;
+		try {
+			inputStream = new FileReader(filename);
+			int c;
+			String result = "";
+			while ((c = inputStream.read())!=-1) {
+				result = result + (char)c;
+			}
+			return result;
+		}
+		catch (IOException e) {
+			return "-1";
+		}
+	}
+	public static String backwardsReadFile(String filename) {
+		FileReader inputStream = null;
+		try {
+			inputStream = new FileReader(filename);
+			int c;
+			String result = "";
+			String store = "";
+			while ((c = inputStream.read())!=-1) {
+				result = result + (char)c;
+			}
+			for (int i = 0;i<result.length();i++) {
+				store = store + result.charAt(result.length()-1-i);
+			}
+			return store;
+		}
+
+		catch (IOException e) {
+			return "-1";
+		}
 	}
 
 	
